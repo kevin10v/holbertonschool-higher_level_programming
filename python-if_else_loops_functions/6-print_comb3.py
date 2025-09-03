ca565002ce5410267d008969efad31c225431f5d
kevin10v <vokakevin@icloud.com>
2025-09-03 10:13:38 +0000

    update

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("{:d}{:d}".format(i, j))
         else:
-            print(f"{i}{j}", end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")

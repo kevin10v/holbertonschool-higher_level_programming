d643daf4c5ea773fea323ee71ae00ad9ef64ffbd
kevin10v <vokakevin@icloud.com>
2025-09-12 09:31:19 +0000

    update

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for add_integer function."""
+
+
+def add_integer(a, b=98):
+    """Return the sum of a and b as integers."""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)

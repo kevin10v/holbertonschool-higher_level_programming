6fc443c6c1c969edf3fa71e3b2d9fbdd5f777e8b
kevin10v <vokakevin@icloud.com>
2025-09-12 09:47:36 +0000

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python3
-"""Simple addition module.
 
-Defines add_integer(a, b=98).
-
-Adds two integers with type checks.
-"""
+"""Module that contain function that adds two integers"""
 
 
 def add_integer(a, b=98):
-    """Add two integers.
-
-    Floats are cast to ints.
-    """
-    if not isinstance(a, (int, float)):
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
-    # Refuzo NaN dhe ±inf pa importe
-    if isinstance(a, float) and (
-            a != a or a in (float('inf'), float('-inf'))):
-        raise TypeError("a must be an integer")
-    if isinstance(b, float) and (
-            b != b or b in (float('inf'), float('-inf'))):
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)

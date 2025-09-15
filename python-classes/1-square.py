a64ec3178e5ac2ec5f2d8692211c3ebe6db7c2e8
kevin10v <vokakevin@icloud.com>
2025-09-15 09:41:49 +0000

    update

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a class Square with a private size."""
+
+
+class Square:
+    """Class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize a new Square with a private size."""
+        self.__size = size

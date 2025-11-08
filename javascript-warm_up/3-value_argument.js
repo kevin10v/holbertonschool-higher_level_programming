e1198b833e3c859b7245420a905c162c1a1ae8a2
kevin10v <vokakevin@icloud.com>
2025-11-08 17:30:18 +0000

    update

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else if (process.argv[3] === undefined) {
-    console.log('Argument found');
+if (process.argv[2]) {
+    console.log(process.argv[2]);
   } else {
-    console.log('Arguments found');
+    console.log('No argument');
   }
-  
